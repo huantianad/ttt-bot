4302594bcdb6dd5305fca10e5884bedbe591518d
huantian <davidtianli@gmail.com>
2021-06-20 14:19:43 -0700

    Rewrote the bot to be in a class

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # ttt-bot
 Simple Tic-Tac-Toe bot written in discord.py
 
-Made in Python 3.8 and discord.py 1.5.1.
+Made in Python 3.9 and discord.py v1.7.3.
 Make sure to set bot token at the end of the code before you run it!
--- main.py
+++ main.py
@@ -1,160 +1,196 @@
+import asyncio
+from typing import Optional, Iterator
+
 import discord
 from discord.ext import commands
 
-emoji_dict = {
-    u"\u2196": 0,
-    u"\u2B06": 1,
-    u"\u2197": 2,
-    u"\u2B05": 3,
-    u"\u23FA": 4,
-    u"\u27A1": 5,
-    u"\u2199": 6,
-    u"\u2B07": 7,
-    u"\u2198": 8
-}
 
-bot = commands.Bot(command_prefix="!")
-color = discord.Color.blue()  # Change this color to read from the config later.
+class TTTGame:
+    def __init__(self, ctx: commands.Context, player_2: discord.Member) -> None:
+        self.ctx = ctx
+        self.player_1 = ctx.author
+        self.player_2 = player_2
+        self.current_player = self.player_1
 
+        self.grid = TTTGrid()
 
-@bot.event
-async def on_ready():
-    print(f"Logged in as {bot.user}!")
+        self.reaction_emojis = {
+            u"\u2196": (0, 0),
+            u"\u2B06": (0, 1),
+            u"\u2197": (0, 2),
+            u"\u2B05": (1, 0),
+            u"\u23FA": (1, 1),
+            u"\u27A1": (1, 2),
+            u"\u2199": (2, 0),
+            u"\u2B07": (2, 1),
+            u"\u2198": (2, 2)
+        }
 
+    async def start(self) -> None:
+        """
+        Main method that is called to start the game.
+        """
 
-def make_grid_internals(grid):
-    """Converts the normal grid list into human-readable version."""
-    # Replace all values in the grid list with emojis
-    new_grid = [":white_large_square:" if (not x)
-                else ":regional_indicator_x:" if x == 1
-                else ":o2:" if x == 2
-                else "?"
-                for x in grid]
+        # Send the initial message
+        self.message: discord.Message = await self.ctx.send(embed=self.make_embed())
 
-    # Adds a new line every three emojis.
-    return "\n".join("".join(new_grid[i:i + 3]) for i in range(0, len(new_grid), 3))
+        # Add the reactions which act as controls
+        for emoji in self.reaction_emojis:
+            await self.message.add_reaction(emoji)
 
+        # do_turn returns self.grid.check_for_end(), so it is None with the game hasn't finished
+        while self.grid.check_for_end() is None:
+            await self.do_turn()
 
-async def draw_grid(channel, grid, player1, player2, current_player):
-    """Draws the initial grid of the game."""
-    grid = make_grid_internals(grid)
+    async def do_turn(self) -> None:
+        reaction, user = await self.ctx.bot.wait_for("reaction_add", check=self.check, timeout=60*60*24)
+        row, col = self.reaction_emojis[reaction.emoji]
 
-    current_player = player1 if current_player == 1 else player2
-    description = f"{player1.mention} vs. {player2.mention}\n{current_player.mention}'s turn!"
+        # Modify the actual grid with the move
+        self.grid.grid[row][col] = 1 if self.current_player == self.player_1 else 2
 
-    embed = discord.Embed(title="Tic-Tac-Toe!", description=description, color=color)
-    embed.add_field(name="Grid", value=grid)
+        # Swap the current player with the other player
+        self.current_player = self.player_1 if self.current_player == self.player_2 else self.player_2
+        await self.message.edit(embed=self.make_embed())
 
-    return await channel.send(embed=embed)
+    def make_embed(self) -> discord.Embed:
+        """
+        Creates an embed that describes the current state of the game.
+        Used to edit the message to show an updated grid + current turn.
 
+        Returns:
+            discord.Embed: The embed.
+        """
 
-async def edit_grid(message, grid, player1, player2, current_player):
-    grid = make_grid_internals(grid)
+        winner = {1: self.player_1, 2: self.player_2, 3: "draw"}.get(self.grid.check_for_end())
+        message = (f"{self.current_player.mention}'s turn!" if winner is None
+                   else f"{winner.mention} has won!" if winner != "draw"
+                   else "It's a draw!")
 
-    current_player = player1 if current_player == 1 else player2
-    description = f"{player1.mention} vs. {player2.mention}\n{current_player.mention}'s turn!"
+        description = f"{self.player_1.mention} vs. {self.player_2.mention}\n{message}"
+        embed = discord.Embed(title="Tic-Tac-Toe!", description=description, color=discord.Color.gold())
+        embed.add_field(name="Grid", value=self.grid.pretty_grid())
 
-    embed = discord.Embed(title="Tic-Tac-Toe!", description=description, color=color)
-    embed.add_field(name="Grid", value=grid)
+        return embed
 
-    await message.edit(embed=embed)
+    def check(self, r: discord.Reaction, u: discord.User) -> bool:
+        """
+        Check to make sure the user is the current player, as well as the reaction is for a valid square.
+        """
 
+        index = self.reaction_emojis.get(r.emoji)
+        if index is None:
+            return
+        row, col = index
 
-async def edit_grid_end(message, grid, player1, player2, current_player, end_message):
-    grid = make_grid_internals(grid)
-
-    current_player = player1 if current_player == 1 else player2
-    description = f"{player1.mention} vs. {player2.mention}\n{end_message}"
-
-    embed = discord.Embed(title="Tic-Tac-Toe!", description=description, color=color)
-    embed.add_field(name="Grid", value=grid)
-
-    await message.edit(embed=embed)
-
-
-def win_indexes(n):
-    # Rows
-    for r in range(n):
-        yield [(r, c) for c in range(n)]
-    # Columns
-    for c in range(n):
-        yield [(r, c) for r in range(n)]
-    # Diagonal top left to bottom right
-    yield [(i, i) for i in range(n)]
-    # Diagonal top right to bottom left
-    yield [(i, n - 1 - i) for i in range(n)]
+        return u == self.current_player and self.grid.grid[row][col] == 0
 
 
-def is_winner(board, decorator):
-    n = len(board)
-    for indexes in win_indexes(n):
-        if all(board[r][c] == decorator for r, c in indexes):
-            return True
-    return False
+class TTTGrid:
+    def __init__(self) -> None:
+        self.grid = [[0] * 3 for _ in range(3)]
+
+        self.grid_emojis = {
+            0: ":white_large_square:",
+            1: ":regional_indicator_x:",
+            2: ":o2:"
+        }
+
+    def pretty_grid(self) -> str:
+        """
+        Creates a stringified grid that uses emojis.
+
+        Returns:
+            str: The stringified grid.
+        """
+
+        str_grid = "\n".join("".join(map(str, row)) for row in self.grid)  # Adds a new line every three emojis.
+        for value, emoji in self.grid_emojis.items():
+            str_grid = str_grid.replace(str(value), emoji)
+
+        return str_grid
+
+    def check_for_end(self) -> Optional[int]:
+        """
+        Checks if the grid is in an end board state, a player has won or if there is a draw.
+        1 or 2 means player 1 or 2 has won, 3 means the game is a draw.
+        Will return None if the game has not finished.
+
+        Returns:
+            Optional[int]: An integer describing the end state.
+        """
+
+        if self.is_winner(1):
+            return 1
+
+        if self.is_winner(2):
+            return 2
+
+        if 0 not in [value for row in self.grid for value in row]:  # All the spots are filled => draw
+            return 3
+
+        return None
+
+    def is_winner(self, player_int: int) -> bool:
+        """
+        Checks if the given player has won.
+
+        Args:
+            player_int (int): Which player to check.
+
+        Returns:
+            bool: If the given player has won.
+        """
+
+        for indexes in self.win_indexes():
+            if all(self.grid[r][c] == player_int for r, c in indexes):
+                return True
+
+        return False
+
+    def win_indexes(self) -> Iterator[Iterator[tuple[int, int]]]:
+        """
+        Creates all the possible combinations of positions that you could win with.
+        For example, will generate all rows on the board, all cols, all diagonals
+
+        Yields:
+            Iterator[Iterator[tuple[int, int]]]: An iterator of iterators of indicies.
+        """
+
+        n = 3  # Number of rows/cols in grid
+
+        for r in range(n):  # Rows
+            yield ((r, c) for c in range(n))
+
+        for c in range(n):  # Columns
+            yield ((r, c) for r in range(n))
+
+        yield ((i, i) for i in range(n))  # Diagonal top left to bottom right
+        yield ((i, n - 1 - i) for i in range(n))  # Diagonal top right to bottom left
 
 
-def check_for_end(grid):
-    if 0 not in grid:
-        return "draw"
-    elif is_winner([grid[i:i + 3] for i in range(0, len(grid), 3)], 1):
-        return 1
-    elif is_winner([grid[i:i + 3] for i in range(0, len(grid), 3)], 2):
-        return 2
-    else:
-        return
+def main():
+    bot = commands.Bot(command_prefix="!")
+
+    @bot.event
+    async def on_ready():
+        print(f"Logged in as {bot.user}!")
+
+    @bot.command()
+    async def ttt(ctx: commands.Context, *, player_2: Optional[discord.Member]):
+        """Start a tic-tac-toe game with another person!"""
+
+        if player_2 is None or player_2.bot or player_2 == ctx.author:
+            await ctx.send("Specify another player.")
+            return
+
+        try:
+            await TTTGame(ctx, player_2).start()
+        except asyncio.TimeoutError:
+            pass
+
+    bot.run("BOT TOKEN HERE")
 
 
-@bot.command()
-async def ttt(ctx, player2: commands.MemberConverter):
-    """Starts a Tic-Tac-Toe game with the specified player!"""
-    player1 = ctx.author
-    current_player = 1
-    running = True
-    used_emojis = []
-
-    # Makes sure that the player isn't a bot.
-    if player2.bot:
-        await ctx.send("You can't play against a bot!")
-        return
-
-    # Creates and sends the initial grid
-    grid = [0 for x in range(9)]
-    message = await draw_grid(ctx.channel, grid, player1, player2, current_player)
-
-    # Adds all the emojis to make moves
-    for emoji in emoji_dict:
-        await message.add_reaction(emoji)
-
-    # Running loop, continuously checks for new inputs until someone wins
-    while running:
-        # Makes a usable object based on current player
-        current_player_object = player1 if current_player == 1 else player2
-
-        def check(r, u):
-            # Checks that it is a valid emoji, that the emoji wasn't used before, and that it is the current player
-            return (r.emoji in emoji_dict) and (r.emoji not in used_emojis) and (u == current_player_object)
-
-        # Waits for a reaction, edits grid and used emojis accordingly
-        reaction, user = await bot.wait_for("reaction_add", check=check)
-        grid[emoji_dict[reaction.emoji]] = 1 if current_player == 1 else 2 if current_player == 2 else "??"
-        used_emojis.append(reaction.emoji)
-
-        end = check_for_end(grid)
-        if end == "draw":
-            await edit_grid_end(message, grid, player1, player2, current_player, "It's a draw!")
-            running = False
-        elif end == 1:
-            await edit_grid_end(message, grid, player1, player2, current_player, f"{player1.mention} has won!")
-            running = False
-        elif end == 2:
-            await edit_grid_end(message, grid, player1, player2, current_player, f"{player2.mention} has won!")
-            running = False
-        else:
-            # Switch current player to next player
-            current_player = 2 if current_player == 1 else 1
-
-            # Update grid with new information
-            await edit_grid(message, grid, player1, player2, current_player)
-
-
-bot.run("PUT BOT TOKEN HERE")
+if __name__ == "__main__":
+    main()
